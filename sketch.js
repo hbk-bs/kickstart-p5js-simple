cc576aecc62df1d54d49e995108fa5ff3d4d6359
ff6347 <hash@ff6347.email>
2023-10-18 16:57:40 +0200

    fix(lib): Path on lib

--- sketch.js
+++ sketch.js
@@ -1,10 +1,3 @@
-/// <reference path="./node_modules/@types/p5/lib/addons/p5.sound.d.ts" />
-/// <reference path="./node_modules/@types/p5/global.d.ts" />
-/// <reference path="./node_modules/@types/p5/literals.d.ts" />
-/// <reference path="./node_modules/@types/p5/constants.d.ts" />
-// @ts-check
-// Keep these comments above alive. They will help you while writing code.
-
 function setup() {
 	// setup runs once
 	const canvas = createCanvas(100, 100);
@@ -14,4 +7,5 @@ function setup() {
 
 function draw() {
 	// draw runs all the time
+	point(50, 50);
 }
